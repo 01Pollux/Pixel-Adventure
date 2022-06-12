25782cbbe0a8adc97257e0cc4877c2db2794e838
Yassine Sen <61026912+01Pollux@users.noreply.github.com>
2022-06-12 15:20:42 +0100

    Add random background for menu updated background's parallax

--- Assets/Scripts/Mechanics/ParallaxBackground.cs
+++ Assets/Scripts/Mechanics/ParallaxBackground.cs
@@ -4,16 +4,38 @@ namespace Mechanics
 {
     public class ParallaxBackground : MonoBehaviour
     {
-        [SerializeField] private float m_RepeatDistance = 24f;
-        [SerializeField] private Vector2 m_MoveDistance = new(1f, -.5f);
-        [SerializeField] private float m_MoveSpeed = 1.5f;
+        [SerializeField] private Vector2 m_ScrollDelta;
+        [SerializeField] private float m_ScrollSpeed;
 
-        private void Update()
+        private Vector2 m_SpriteSize;
+        private Vector2 m_StartPosition;
+
+        private void Start()
         {
-            transform.localPosition += (Vector3)(m_MoveSpeed * Time.deltaTime * m_MoveDistance);
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            Texture texture = sprite.texture;
 
-            if (Vector2.Distance(transform.localPosition, Vector2.zero) > m_RepeatDistance)
-                transform.localPosition = Vector3.zero;
+            m_SpriteSize.x = texture.width / sprite.pixelsPerUnit;
+            m_SpriteSize.y = texture.height / sprite.pixelsPerUnit;
+
+            m_StartPosition = transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            transform.position += (Vector3)m_ScrollDelta * m_ScrollSpeed;
+            Vector2 move_offset = m_StartPosition - (Vector2)transform.position;
+
+            if (Mathf.Abs(move_offset.x) >= m_SpriteSize.x)
+            {
+                float delta_x = move_offset.x % m_SpriteSize.x;
+                transform.position = new Vector3(m_StartPosition.x + delta_x, transform.position.y);
+            }
+            if (Mathf.Abs(move_offset.y) >= m_SpriteSize.y)
+            {
+                float delta_x = move_offset.y % m_SpriteSize.y;
+                transform.position = new Vector3(transform.position.x, m_StartPosition.y + delta_x);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Mechanics/RandomBackgroundSelector.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+namespace Mechanics
+{
+    public class RandomBackgroundSelector : MonoBehaviour
+    {
+        [SerializeField] Sprite[] m_Sprites;
+
+        private void Awake()
+        {
+            SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer>();
+            sprite_renderer.sprite = m_Sprites[Random.Range(0, m_Sprites.Length - 1)];
+        }
+    }
+}
